d4bac51139c649b3feeef605103fd97806435d88
yishant11 <yishant11@example.com>
2026-05-23 17:39:15 +0530

    long pooling

--- index.js
+++ index.js
@@ -13,6 +13,33 @@ setTimeout(() => {
   console.log("Version updated");
 }, 15000);
 
+
+let message = null;
+
+app.get("/updates", (req, res) => {
+  console.log("Client connected");
+
+  const interval = setInterval(() => {
+    if (message) {
+      res.json({ message });
+
+      message = null;
+      clearInterval(interval);
+    }
+  }, 1000);
+
+  // Timeout after 30 sec
+  req.on("close", () => {
+    clearInterval(interval);
+  });
+});
+
+// Simulate new data after 10 sec
+setTimeout(() => {
+  message = "New Notification!";
+  console.log("New data available");
+}, 10000);
+
 app.listen(3005, () => {
   console.log("Server running on port 3005");
 });
